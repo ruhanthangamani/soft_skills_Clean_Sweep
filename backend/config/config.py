5a77d369c2a0778d4a1c34fe20841efbdc537092
ruhanthangamani <ruhanthangamani@gmail.com>
2026-03-02 23:54:57 +0530

    Switch to Supabase direct port 5432

--- backend/config/config.py
+++ backend/config/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_secret_key_clean_sweep'
-    SUPABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres.qwhngsneiryznespypkn:Ruhan%40221006@aws-0-ap-south-1.pooler.supabase.com:6543/postgres?sslmode=require'
+    SUPABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres.qwhngsneiryznespypkn:Ruhan%40221006@aws-0-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require'
