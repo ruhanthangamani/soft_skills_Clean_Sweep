8b5e18b18ad5287f663c52ae22c7e03f66a94a6a
ruhanthangamani <ruhanthangamani@gmail.com>
2026-03-03 00:34:42 +0530

    Use exact region connection string provided by user

--- backend/config/config.py
+++ backend/config/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_secret_key_clean_sweep'
-    SUPABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:Ruhan%40221006@db.qwhngsneiryznespypkn.supabase.co:5432/postgres'
+    SUPABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres.qwhngsneiryznespypkn:Ruhan%40221006@aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres?sslmode=require'
